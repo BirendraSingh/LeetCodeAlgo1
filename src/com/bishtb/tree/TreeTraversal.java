cec2327b795eeb43d81aa5bc35982824697bdd4b
Birendra Bisht <birendrasingh.bisht@yahoo.com>
2022-07-26 13:27:27 +0530

    PostOrder tree traversal(DFS) with recurssion.

--- src/com/bishtb/tree/TreeTraversal.java
+++ src/com/bishtb/tree/TreeTraversal.java
@@ -18,6 +18,9 @@ public class TreeTraversal {
         System.out.println("**** InOrder *****");
         for(int i: new TreeTraversal().inOrder(root))
         	System.out.println(i);
+        System.out.println("**** PostOrder *****");
+        for(int i: new TreeTraversal().postorderTraversal(root))
+        	System.out.println(i);
 	}
 
 	//Preorder ( Root, Left, Right) traversal of a tree (DFS)
@@ -40,4 +43,14 @@ public class TreeTraversal {
 		result.addAll(inOrder(root.right));
 		return result;
 	}
+
+	public List<Integer> postorderTraversal(TreeNode root) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		if(root == null)
+			return result;
+		result.addAll(postorderTraversal(root.left));
+		result.addAll(postorderTraversal(root.right));
+		result.add(root.val);
+		return result;
+    }
 }
