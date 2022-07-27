a9a4ec0c7ba080212614112034665bad4d32d202
Birendra Bisht <birendrasingh.bisht@yahoo.com>
2022-07-27 12:51:30 +0530

    Tree Level order traversal(BFS).

--- src/com/bishtb/tree/TreeTraversal.java
+++ src/com/bishtb/tree/TreeTraversal.java
@@ -1,7 +1,9 @@
 package com.bishtb.tree;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class TreeTraversal {
 
@@ -21,6 +23,11 @@ public class TreeTraversal {
         System.out.println("**** PostOrder *****");
         for(int i: new TreeTraversal().postorderTraversal(root))
         	System.out.println(i);
+        System.out.println("**** Level order *****");
+        for(List<Integer> list: new TreeTraversal().levelOrder(root)) {
+        	for(int i: list)
+        		System.out.println(i);
+        }
 	}
 
 	//Preorder ( Root, Left, Right) traversal of a tree (DFS)
@@ -52,5 +59,24 @@ public class TreeTraversal {
 		result.addAll(postorderTraversal(root.right));
 		result.add(root.val);
 		return result;
+    }
+
+	public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        if(root == null) return result;
+        queue.offer(root);
+        while(!queue.isEmpty()) {
+        	List<Integer> resultList = new ArrayList<Integer>();
+            int level = queue.size();
+            for(int i=0; i<level;i++) {
+            	TreeNode node = queue.poll();
+        	    resultList.add(node.val);
+                if(node.left!=null) queue.offer(node.left);
+                if(node.right!=null) queue.offer(node.right);
+            }
+            result.add(resultList);
+        }
+        return result;
     }
 }
